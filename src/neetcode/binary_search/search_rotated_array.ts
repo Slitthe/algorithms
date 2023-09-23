bf6c4f5faa43cffc1a41aeb92adb5fee6cae9fab
Silviu <sil.gherman@gmail.com>
2023-09-23 19:35:23 +0300

    Search for min in rotated array

---
+++ src/neetcode/binary_search/search_rotated_array.ts
@@ -0,0 +1,16 @@
+export function searchRotatedArray(arr: number[], left: number = 0, right: number = arr.length - 1): number {
+    if (left === right) {
+        return arr[left];
+    }
+
+    const middle = Math.floor((right - left) / 2) + left;
+
+    let leftMin = Math.min(arr[left], arr[middle]);
+    let rightMin = Math.min(arr[middle + 1], arr[right]);
+
+    if (leftMin < rightMin) {
+        return searchRotatedArray(arr, left, middle)
+    } else {
+        return searchRotatedArray(arr, middle + 1, right);
+    }
+}
